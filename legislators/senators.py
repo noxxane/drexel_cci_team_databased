45283f9e5a0d981675dabaa8a151ff09331b2c72
noxxane <colie572@gmail.com>
2024-07-21 10:22:37 -0400

    fixed senators.py to include the senator's party

--- legislators/senators.py
+++ legislators/senators.py
@@ -10,9 +10,13 @@ senators = [x for x in legislators if x["terms"][-1]["type"] == "sen"]
 senators_dict = {}
 for senator in senators:
     name = f"{senator["name"]["first"]} {senator["name"]["last"]}"
-    if senator["terms"][-1]["state"] not in senators_dict:
-        senators_dict[senator["terms"][-1]["state"]] = []
-    senators_dict[senator["terms"][-1]["state"]].append(name)
+    current_term = senator["terms"][-1]
+    state = current_term["state"]
+    party = current_term["party"]
+    if current_term["state"] not in senators_dict:
+        senators_dict[state] = []
+    print(f"{name}, {party}")
+    senators_dict[state].append(f"{name}, {party}")
 
 with open("../states_to_sens.json", "w", encoding="utf-8") as out_file:
     json.dump(senators_dict, out_file)
