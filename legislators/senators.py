681703fe6d9edb8c71396420115fcc54077f38f4
noxxane <colie572@gmail.com>
2024-07-21 17:18:38 -0400

    added info

--- legislators/senators.py
+++ legislators/senators.py
@@ -9,10 +9,10 @@ legislators = json.loads(json_content)
 senators = [x for x in legislators if x["terms"][-1]["type"] == "sen"]
 senators_dict = {}
 for senator in senators:
-    name = f"{senator["name"]["first"]} {senator["name"]["last"]}"
+    name: str = f"{senator["name"]["first"]} {senator["name"]["last"]}"
     current_term = senator["terms"][-1]
-    state = current_term["state"]
-    party = current_term["party"]
+    state: str = current_term["state"]
+    party: str = current_term["party"]
     if current_term["state"] not in senators_dict:
         senators_dict[state] = []
     print(f"{name}, {party}")
