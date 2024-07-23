214b51cb6013a1f6a2a30dd99b83be02ce874559
noxxane <colie572@gmail.com>
2024-07-22 20:48:30 -0400

    don't mind me just imposing my ironclad will upon this innocent little website

--- src/script.js
+++ src/script.js
@@ -1,24 +1,33 @@
 async function getRep(userZip) {
-  const response = await fetch("../zips_to_reps.json");
-  const json = await response.json();
-  return json[userZip];
+    const response = await fetch("../zips_to_reps.json");
+    const json = await response.json();
+    return json[userZip];
 }
 
-async function getSen(userSen){
-  const response = await fetch("../states_to_sens.json");
-  const json = await response.json();
-  return json[userSen];
+async function getSen(userSen) {
+    const response = await fetch("../states_to_sens.json");
+    const json = await response.json();
+    return json[userSen];
 }
 
 async function findRep() {
-  let userZip = document.getElementById("zip_input").value;
-  document.getElementById("rep_output").innerHTML = await getRep(userZip);
-  
-  // alert(await getRep(userZip)); 
+    let userZip = document.getElementById("zip_input").value;
+    reps = await getRep(userZip);
+    if (reps.length == 1) {
+        output_span = reps[0]
+    } else if (reps.length == 2) {
+        output_span = `${reps[0]} and ${reps[1]}`
+    } else if (reps.length == 3) {
+        output_span = `${reps[0]}, ${reps[1]}, and ${reps[2]}`
+    } else {
+        output_span = reps
+    }
+    document.getElementById("rep_output").innerHTML = output_span;
 }
 
 async function findSen() {
-  let userState = document.getElementById("state_input").value;
-  document.getElementById("sen_output").innerHTML = await getSen(userState);
-  // alert(await getSen(userState));
-  }
+    let userState = document.getElementById("state_input").value;
+    senators = await getSen(userState);
+    output_span = `${senators[0]} and ${senators[1]}`;
+    document.getElementById("sen_output").innerHTML = output_span;
+}
